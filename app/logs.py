ea6a9e92f137026ad40245a175dc5fe9b05100bc
Yahir Orozco <yorozcolop49@gmail.com>
2026-07-03 12:25:38 -0700

    allowed food_id in log and date to be accessed

--- app/logs.py
+++ app/logs.py
@@ -2,6 +2,13 @@ from datetime import date
 import uuid
 class Log: 
     def __init__(self, food_id):
-        self.food_id = food_id
-        self.date = date.today().isoformat()
+        self._food_id = food_id
+        self._date = date.today().isoformat()
 
+    @property
+    def food_id(self):
+        return self._food_id
+    
+    @property
+    def date(self):
+        return self._date
